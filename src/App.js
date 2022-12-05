e6b8b668c63437f0d00c5eb4b34d08154da45cbb
Shishir Patel <shishirpatel@gmail.com>
2022-12-05 11:02:12 +0000

    update for main

--- src/App.js
+++ src/App.js
@@ -1,47 +1,35 @@
 import './App.css';
 import './App.scss';
-import swingIconMobile from './Images/swingIconMobile.svg'
-import footballIconMobile from './Images/footballIconMobile.svg'
-import jumppadIconMobile from './Images/jumppadIconMobile.svg'
-import swingIconMobile2 from './Images/swingIconMobile2.svg'
-import footballIconMobile2 from './Images/footballIconMobile2.svg'
-import jumppadIconMobile2 from './Images/jumppadIconMobile2.svg'
 import logo from './Images/PLAYGROUNDS.svg'
 import logo2 from './Images/PLAY.svg'
 import React, {useState, useEffect} from 'react';
 import Dropdown from './Components/Dropdown/Dropdownbtn.js'
 import supabase from './supabaseClient';
 import Api from './Components/Cards/api';
+import Icons from './Components/Icons/icons'
 
 function App() {
 // These states are used as props in the api and dropdown components here below
   const [playgrounds, setPlaygrounds] = useState([]); 
+  const [filter, setFilter] = useState({}); 
   const [filterPlaygrounds, setFilterPlaygrounds] = useState([]); 
   console.log(filterPlaygrounds);
+  console.log(filter);
 
-/*Clickable feature of swing, football and jumppad icons*/
-  const [sIcon, setsIcon] = useState(swingIconMobile);
-  const changesIcon = ()=>{
-    if (sIcon.includes('Mobile2')){
-      setsIcon(swingIconMobile)}
-    else {setsIcon(swingIconMobile2)}
-  }
-
-  const [fIcon, setfIcon] = useState(footballIconMobile);
-  const changefIcon = ()=>{
-    if (fIcon.includes('Mobile2')){
-      setfIcon(footballIconMobile)}
-    else {setfIcon(footballIconMobile2)}
-  }
-
-  const [jIcon, setjIcon] = useState(jumppadIconMobile);
-  const changejIcon = ()=>{
-    if (jIcon.includes('Mobile2')){
-      setjIcon(jumppadIconMobile)}
-    else {setjIcon(jumppadIconMobile2)}
-    }
 
 console.log (window.screen.width)
+
+useEffect (()=> {
+  const postalSelect = playgrounds.filter(playground => {
+    const postal=filter.postal===undefined || playground.postal == filter.postal
+    const type=filter.type===undefined || playground.type == filter.type
+    return (postal && type)
+  });
+  setFilterPlaygrounds(postalSelect)
+//run  useEffect everytime fitler changes
+},[filter]
+)
+
   return (
     <div className='App'>
       <div className='nav-bar'>
@@ -55,24 +43,14 @@ console.log (window.screen.width)
         </div>
         <div className='Dropdown'>
           {/* this is referencing the compnent in dropdown.js */}
-          <Dropdown playgrounds={playgrounds} setFilterPlaygrounds={setFilterPlaygrounds} />
+          <Dropdown playgrounds={filterPlaygrounds} setFilter={setFilter} filter={filter}/>
       </div>
       </div>
-      <div className='Icons'>
-        <div className='grow'>
-          <div className='swing'>
-            <img onClick={changesIcon} src={sIcon}></img>
-          </div>
-          <div className='football'>
-            <img onClick={changefIcon} src={fIcon}></img>
-          </div>
-          <div className='jumppad'>
-            <img onClick={changejIcon} src={jIcon}></img>
-          </div>
-        </div>
-      </div>
+
+      <Icons playgrounds={filterPlaygrounds} setFilter={setFilter} filter={filter}/>
+
       <div className='Card'>
-        {/* this is referencing the compnent in api.js (props used in api.js) */}
+        {/* this is referencing the component in api.js (props used in api.js) */}
         <Api playgrounds={filterPlaygrounds} setPlaygrounds={setPlaygrounds} setFilterPlaygrounds={setFilterPlaygrounds} hasFilter={playgrounds.length!=filterPlaygrounds.length}/>
       </div>
     </div>
