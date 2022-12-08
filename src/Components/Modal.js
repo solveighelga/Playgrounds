3f6b62a39bdd3a76f0ea2b03c5e3720c3c6484d5
Lydia <lydiadoula@gmail.com>
2022-12-08 09:41:22 +0000

    popup

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Modal.css'
 
+
 const Modal = ({ data, close}) => {
   return (
     <div className='modal'>
-      <h1 className='address'>{data.address}</h1>      
-      <img className='photo' src={data.photoURL}></img>
+      <h1 className='address'>{data.address}</h1>  
+      <div className='thePhotos'>    
+        <img className='photo' src={data.photoURL}></img>
+      </div>
+      <h2 className='equipment'>Equipment</h2>
       <div className='icons10'>
         <img className='regularswing' src={data.regularSwing}></img>
         <img className='slide' src={data.slide}></img>
         <img className='rocker' src={data.springRocker}></img>
+      </div>
+      <div className='themap'>        
+        <img className='googlemap' src={data.lat.lng}></img> 
       </div>
     </div>
   )
