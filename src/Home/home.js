cbd39340eed1ea56fd25ec26efbda92d4bd37940
Lydia <lydiadoula@gmail.com>
2022-12-08 14:05:59 +0000

    changed Home.js

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  {useEffect, useState} from 'react';
 import './home.scss';
 import './Intro/splash';
 import kid from './kid.png';
@@ -7,9 +7,10 @@ import mobile from './mobile1.png';
 import arrow from './down.png';
 import Splash from './Intro/splash';
 
-const Home = function(setRoute) {
-    
-    
+
+const Home = function() {
+    const [open, setOpen] = useState(false); // By default the drop down is not open (dropdown open = false)
+    /*const [rotate, setRotate] = useState(false);*/
 
     return (
     <div id="main">
@@ -35,28 +36,41 @@ const Home = function(setRoute) {
             id="image2"
             src={mobile}
         />
-        {/*
-        <img
-            alt='Logo' 
-            id="logo"
-            src={logo}
-        />    
-    */}
+        
+ 
         <Splash>
 
         </Splash>
 
-        <button class="start">       
-            Select City
-            <img
-                alt='Arrow' 
-                id="down"
-                src={arrow}
-            />
-        </button> 
-      
-    </div>
-  </div>
+        </div> 
+        <div className='menu-container1'>
+            <div className='menu-trigger1' onClick={() =>{setOpen(!open)/*, setRotate(!rotate)*/}}> Select City
+        
+                <img /*className={`imgrotate ${rotate? 'active' : 'inactive'}`}*/
+
+                    alt='Arrow' 
+                    id="down"
+                    src={arrow}
+                />
+            </div>
+            <div className={`dropdown-menu1 ${open? 'active' : 'inactive'}`}>
+                <ul>
+                    <li><DropdownItem1 text = {'Reykjavík'} /></li>
+                </ul>
+            </div>
+        </div>
+        </div>
     );
 }
+      
+
+    function DropdownItem1(props){
+        return(
+        <li className='dropdownItem1'>
+            <a>{props.text}</a>
+        </li>
+        );
+    }
+
 export default Home;
+      
