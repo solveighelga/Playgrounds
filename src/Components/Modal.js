7feb05264cefa154f83019752af8725e236188f3
Shishir Patel <shishirpatel@gmail.com>
2022-12-08 16:37:20 +0000

    icon update

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -14,9 +14,21 @@ const Modal = ({ data, close}) => {
       </div>
       <h2 className='equipment'>Equipment</h2>
       <div className='icons10'>
-        <img className='regularswing' src={data.regularSwing}></img>
+      <img className='regularswing' src={data.regularSwing}></img>
         <img className='slide' src={data.slide}></img>
         <img className='rocker' src={data.springRocker}></img>
+        <img className='infantSwing' src={data.infantSwing}></img>
+        <img className='basketSwing' src={data.basketSwing}></img>
+        <img className='tireSwing' src={data.tireSwing}></img>
+        <img className='playhouse' src={data.playhouse}></img>
+        <img className='playhousePole' src={data.playhousePole}></img>
+        <img className='CastleSlide' src={data.CastleSlide}></img>
+        <img className='castleMonkeyBars' src={data.castleMonkeyBars}></img>
+        <img className='climbingNet' src={data.climbingNet}></img>
+        <img className='seasaw' src={data.seasaw}></img>
+        <img className='carousel' src={data.carousel}></img>
+        <img className='trampoline' src={data.trampoline}></img>
+        <img className='bench' src={data.bench}></img>
       </div>
 
       <div className='themap'>    
