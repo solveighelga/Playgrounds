a70c7b823063a5c9fc1f0b08b99bee19377ce754
solveighelga <111748545+solveighelga@users.noreply.github.com>
2023-04-05 11:18:56 +0000

    created a react app and starter page

---
+++ Documents/GitHub/Module7/framermotion/src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import styled from "styled-components";
+import { Navbar } from "./components/navbar";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(15, 15, 15, 1);
+  color: #fff;
+`;
+
+const InnerContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2em;
+`;
+
+const SomeContent = styled.h2`
+  color: #fff;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <Navbar />
+      <InnerContainer>
+        <SomeContent>Some Content!</SomeContent>
+      </InnerContainer>
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ Documents/GitHub/Module7/framermotion/src/components/navbar/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavbarContainer = styled.div`
+  width: 100%;
+  height: 55px;
+  border-bottom: 1px solid #fff;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  padding: 0 1.5em;
+`;
+
+export function Navbar(props) {
+  return (
+    <NavbarContainer>
+      Navbar
+    </NavbarContainer>
+  );
+}
